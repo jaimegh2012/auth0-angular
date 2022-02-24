d717b4c3db79b6b3484bc6029513b76c5012b497
Jaime Gutierrez <jaime.gutierrez@cit.hn>
2022-02-23 23:08:19 -0600

    completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivateComponent } from './components/private/private.component';
+import { PublicComponent } from './components/public/public.component';
+import { AuthGuard } from './guards/auth.guard';
+//import { AuthGuard } from '@auth0/auth0-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'public', component: PublicComponent},
+  {path: 'private', component: PrivateComponent, canActivate: [AuthGuard]},
+  {path: '**', pathMatch: 'full', redirectTo: 'public'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './components/public/public.component';
+import { PrivateComponent } from './components/private/private.component';
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    PrivateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     // Import the module into the application, with configuration
+     AuthModule.forRoot({
+      domain: 'dev-375cz1aj.us.auth0.com',
+      clientId: 'Yi9CisA5pVtq5hHQU0o78CXx7iBkYSmA',
+      cacheLocation: 'localstorage',
+      useRefreshTokens: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
